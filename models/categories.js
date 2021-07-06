73edaa415e3f08ed29cd98bac002d18269d076d4
timmakokha <makokhatimothy2@gmail.com>
2021-07-06 08:29:43 +0300

    2.0

--- models/categories.js
+++ models/categories.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
+    name: {
+        type: String, 
+        required: true,
+    },
+    icon: {
+        type: String
+    },
+    color: {
+        type: String
+    }
+
     
 
 
--- routers/categories.js
+++ routers/categories.js
@@ -8,7 +8,43 @@ router.get(`/`, async (req,res)=>{
     if(!categoryList) {
         res.status(500).json({success: false})
     }
-        res.send(productList); 
+        res.status(200).send(categoryList); 
+    })
+
+    router.get('/:id', async(req,res)=>{
+        const category = await Category.findById(req.params.id);
+    
+
+    if(!Category) {
+        res.status(500).json({message: 'category with the given ID was not found'})
+    }
+    res.status(200).send(category);
+ }) 
+
+    router.post('/', async (req,res)=>{
+        let category = new Category({
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color
+        })
+        category = await category.save();
+
+        if(!category)
+        return res.status(400).send( 'the category cannot be created')
+
+        res.send(category);
+    })
+
+    router.delete('/:id', (req,res)=>{
+        Category.findByIdAndRemove(req.params.id).then(category =>{
+            if(category) {
+                return res.status(200).json({success: true, message: "category is deleted"})
+            } else{
+                return res.status(404).json({success: false, message: "category not found"})
+            }
+        }).catch(err=>{
+            return res.status(400).json({success: false, error: err})
+        })
     })
 
 module.exports = router;
